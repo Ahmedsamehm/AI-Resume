d4fc0b8b78285ecef852add415fabd2af3410c8f
Ahmedsamehm <a7medxd992@hotmail.com>
2024-10-20 04:48:43 +0300

    first commit

---
+++ src/api/resum-id/routes/resum-id.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * resum-id router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::resum-id.resum-id');
---
+++ src/api/resum-id/services/resum-id.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * resum-id service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::resum-id.resum-id');
